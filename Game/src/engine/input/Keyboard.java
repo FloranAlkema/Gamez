b13648015070b94e7f2576e7c3cd0976568c9c84
FloranAlkema <f_alkema@live.nl>
2014-11-27 11:38:51 +0100

    KeyEvents added

--- Game/src/engine/input/Keyboard.java
+++ Game/src/engine/input/Keyboard.java
@@ -5,29 +5,47 @@ import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
 	private final boolean[] down;
-	
+
 	public Keyboard() {
 		down = new boolean[256];
 	}
-	
+
 	public boolean isDown(final int code) {
 		return down[code];
 	}
-	
+
 	@Override
 	public void keyPressed(final KeyEvent e) {
 		down[e.getKeyCode()] = true;
+		if (e.equals(KeyEvent.VK_A)) {
+			
+		} else if (e.equals(KeyEvent.VK_W)) {
+
+		} else if (e.equals(KeyEvent.VK_D)) {
+
+		} else if (e.equals(KeyEvent.VK_S)) {
+
+		}
 	}
 
 	@Override
 	public void keyReleased(final KeyEvent e) {
 		down[e.getKeyCode()] = false;
+		if (e.equals(KeyEvent.VK_A)) {
+
+		} else if (e.equals(KeyEvent.VK_W)) {
+
+		} else if (e.equals(KeyEvent.VK_D)) {
+
+		} else if (e.equals(KeyEvent.VK_S)) {
+
+		}
 	}
 
 	@Override
 	public void keyTyped(final KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
