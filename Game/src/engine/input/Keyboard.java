bbac95b70f3fd68ba59f0f535360c4685dc83fe9
rerennnn <rehij@hotmail.com>
2014-12-02 09:56:58 +0100

    comment

--- Game/src/engine/input/Keyboard.java
+++ Game/src/engine/input/Keyboard.java
@@ -23,16 +23,16 @@ public class Keyboard implements KeyListener {
 		System.out.println(e.getKeyCode());
 		if (down[65]) {
 			Player.left(true);
-			System.out.println("left");
+			//System.out.println("left");
 		} else if (down[87]) {
 			Player.up(true);
-			System.out.println("up");
+			//System.out.println("up");
 		} else if (down[83]) {
 			Player.down(true);
-			System.out.println("down");
+			//System.out.println("down");
 		} else if (down[68]) {
 			Player.right(true);
-			System.out.println("right");
+			//System.out.println("right");
 		}
 	}
 
@@ -41,19 +41,19 @@ public class Keyboard implements KeyListener {
 		down[e.getKeyCode()] = false;
 		if (!down[65]) {
 			Player.left(false);
-			System.out.println("not left");
+			//System.out.println("not left");
 		} 
 		if (!down[87]) {
 			Player.up(false);
-			System.out.println("not up");
+			//System.out.println("not up");
 		}
 		if (!down[83]) {
 			Player.down(false);
-			System.out.println("not down");
+			//System.out.println("not down");
 		} 
 		if (!down[68]) {
 			Player.right(false);
-			System.out.println("not right");
+			//System.out.println("not right");
 		}
 	}
 
