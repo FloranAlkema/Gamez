88d717aabe4470d3760768a9ca4dc8a84b76d26c
FloranAlkema <f_alkema@live.nl>
2015-02-17 12:25:32 +0100

    Added entities

---
+++ Game/src/engine/scene/entity/GrassBlock.java
@@ -0,0 +1,36 @@
+package engine.scene.entity;
+
+import engine.Game;
+import engine.graphics.Sprite;
+
+public class GrassBlock extends Entity {
+	private final Sprite sprite;
+
+	public GrassBlock() {
+		this(0, 0);
+		type = "GrassBlock";
+	}
+
+	public GrassBlock(int x, int y) {
+		this(x, y, "grass_block.png");
+		type = "GrassBlock";
+	}
+
+	public GrassBlock(int x, int y, String sprite) {
+		this.sprite = Sprite.get("/" + sprite);
+		this.x = x;
+		this.y = y;
+		width = this.sprite.width;
+		height = this.sprite.height;
+	}
+
+	@Override
+	public void update(final Game game) {
+
+	}
+
+	@Override
+	public void render(final Game game) {
+		game.screen.render(sprite, x, y);
+	}
+}
---
+++ Game/src/engine/scene/entity/Lava.java
@@ -0,0 +1,36 @@
+package engine.scene.entity;
+
+import engine.Game;
+import engine.graphics.Sprite;
+
+public class Lava extends Entity {
+	private final Sprite sprite;
+
+	public Lava() {
+		this(0, 0);
+		type = "Lava";
+	}
+
+	public Lava(int x, int y) {
+		this(x, y, "lava.png");
+		type = "Lava";
+	}
+
+	public Lava(int x, int y, String sprite) {
+		this.sprite = Sprite.get("/" + sprite);
+		this.x = x;
+		this.y = y;
+		width = this.sprite.width;
+		height = this.sprite.height;
+	}
+
+	@Override
+	public void update(final Game game) {
+
+	}
+
+	@Override
+	public void render(final Game game) {
+		game.screen.render(sprite, x, y);
+	}
+}
